8ec05fe91cdcbbf946c5df0046a5b407bc15ea1f
kdoroszko <doroszko.k@gmail.com>
2020-04-08 00:28:25 +0200

    Try send file

--- messenger/src/chat_client.cpp
+++ messenger/src/chat_client.cpp
@@ -9,8 +9,13 @@
 //
 
 #include <cstdlib>
+#include <cstring>
 #include <deque>
+#include <experimental/filesystem>
+#include <fstream>
+#include <iomanip>
 #include <iostream>
+#include <string>
 #include <thread>
 #include <boost/asio.hpp>
 #include "chat_message.hpp"
@@ -22,6 +27,8 @@ typedef std::deque<chat_message> chat_message_queue;
 class chat_client
 {
 public:
+  enum { send_msg_cmd_length = 10 };
+
   chat_client(boost::asio::io_context& io_context,
       const tcp::resolver::results_type& endpoints)
     : io_context_(io_context),
@@ -100,24 +107,80 @@ private:
 
   void do_write()
   {
+    std::string send_file_(write_msgs_.front().body(), send_msg_cmd_length);
+
+    if((send_file_.compare(std::string{"/sendfile "})) == 0)
+    {
+      std::size_t path_length = std::strlen(write_msgs_.front().body() + send_msg_cmd_length);
+      std::string path_to_file(write_msgs_.front().body() + send_msg_cmd_length, path_length - 1);
+      //TODO: oblsuzyc brak podania sciezki
+      // std::cout << std::quoted(path_to_file_);
+      std::fstream file_to_send(path_to_file);
+      std::experimental::filesystem::path path = path_to_file; //TODO: obsluzyc wyjatki, gdy nie ma takiej sciezki
+      std::size_t file_size = std::experimental::filesystem::file_size(path);
+
+      char temp_header[chat_message::header_length + 1] = "";
+      std::sprintf(temp_header, "%4d", static_cast<int>(file_size));
+      std::string new_header(temp_header);
+
+      file_to_send.open(path_to_file, std::ios::in);
+      std::string file_message;
+      while(!file_to_send.eof())
+        file_to_send >> file_message;
+      file_to_send.close();
+      std::string header_with_file = temp_header + file_message;
+      std::fstream send(header_with_file);
+
+      do_write_file(send, chat_message::header_length + file_size);
+      //TODO: dokonczyc metode wysylajaca pliki, dopisac metode zapisujaca plik na dysku
+    }
+    else
+    {
+      do_write_text();
+    }
+  }
+
+  void do_write_file(std::fstream& new_file, std::size_t new_length)
+  {
     boost::asio::async_write(socket_,
-        boost::asio::buffer(write_msgs_.front().data(),
-          write_msgs_.front().length()),
-        [this](boost::system::error_code ec, std::size_t /*length*/)
-        {
-          if (!ec)
+          boost::asio::buffer(new_file.rdbuf(), new_length),
+          [this](boost::system::error_code ec, std::size_t /*length*/)
           {
-            write_msgs_.pop_front();
-            if (!write_msgs_.empty())
+            if (!ec)
             {
-              do_write();
+              write_msgs_.pop_front();
+              if (!write_msgs_.empty())
+              {
+                do_write();
+              }
             }
-          }
-          else
+            else
+            {
+              socket_.close();
+            }
+          });
+  }
+
+  void do_write_text()
+  {
+    boost::asio::async_write(socket_,
+          boost::asio::buffer(write_msgs_.front().data(),
+            write_msgs_.front().length()),
+          [this](boost::system::error_code ec, std::size_t /*length*/)
           {
-            socket_.close();
-          }
-        });
+            if (!ec)
+            {
+              write_msgs_.pop_front();
+              if (!write_msgs_.empty())
+              {
+                do_write();
+              }
+            }
+            else
+            {
+              socket_.close();
+            }
+          });
   }
 
 private:
--- messenger/tst/messenger_test.cpp
+++ messenger/tst/messenger_test.cpp
@@ -1,2 +1,3 @@
 #include "gtest/gtest.h"
 
+//TODO: zaimplementowac testy
